6db8c01cbbc21f4a52006729edb192ff65de81d1
wzb <2482924813@qq.com>
2025-10-10 15:54:08 +0800

    JavaScript输入输出

--- js/js/JavaScript初体验/02.输入输出.js
+++ js/js/JavaScript初体验/02.输入输出.js
@@ -1,3 +1,5 @@
-document.write("<p>document.write()方法可以在页面中打印输出内容，并且可以使用HTML标签。并且HTML标签可以被CSS控制，但是已经弃用</p>")
-alert("alert()是页面弹出警告")
+document.write("<p>document.write()方法可以在页面中打印输出内容，并且可以使用HTML标签。并且HTML标签可以被CSS控制，但是已经弃用</p>");
+alert("alert()是页面弹出警告");
 console.log("console.log()是在控制台中输出内容，主要是用于开发者调试");
+prompt("How old are you");
+document.write("<p>和其他语言一样，JavaScript也是从上往下依次执行，但是alert和prompt这样在页面中使用警示框的代码会优先执行</p>");
