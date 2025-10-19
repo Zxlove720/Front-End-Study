b65ee082492cabc0c0f2c9b951f2a5aae7fc939a
wzb <2482924813@qq.com>
2025-10-19 12:14:25 +0800

    JavaScript程序结构案例

--- js/js/JavaScript程序结构/JavaScript程序结构案例.js
+++ js/js/JavaScript程序结构/JavaScript程序结构案例.js
@@ -39,15 +39,45 @@
 //     document.write("运算符输入违法")
 // }
 
-// while求偶数和
-let start = +prompt("请输入起始数字")
-let end = +prompt("请输入末尾数字")
-let i = start
-let sum = 0
-while (i <= end) {
-  if (i % 2 == 0) {
-    sum += i
+// // while求偶数和
+// let start = +prompt("请输入起始数字")
+// let end = +prompt("请输入末尾数字")
+// let i = start
+// let sum = 0
+// while (i <= end) {
+//   if (i % 2 == 0) {
+//     sum += i
+//   }
+//   i++
+// }
+// document.write(`${start}和${end}之间的偶数和为${sum}`)
+
+// 简易ATM机
+let summary = 0
+let money = 0
+let flag = 1
+while (flag) {
+  let choice = +prompt("请选择操作：\n1.存钱\n2.取钱\n3.查看余额\n4.退出")
+  switch (choice) {
+    case 1:
+      money = +prompt("请输入要存入的金额")
+      summary += money
+      break
+    case 2:
+      money = +prompt("请输入要取出的金额")
+      if (summary < money) {
+        alert("余额不足")
+        continue
+      }
+      summary -= money
+      break
+    case 3:
+      alert(`您的余额还有${summary}`)
+      break
+    case 4:
+      flag = 0
+      break
+    default:
+      alert("请输入合法的操作")
   }
-  i++
 }
-document.write(`${start}和${end}之间的偶数和为${sum}`)
