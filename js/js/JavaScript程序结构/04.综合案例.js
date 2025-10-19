a49437fc315281d3849e6b4d4eede2ec73e1723c
wzb <2482924813@qq.com>
2025-10-19 17:56:37 +0800

    JavaScript综合案例

---
+++ js/js/JavaScript程序结构/04.综合案例.js
@@ -0,0 +1,15 @@
+let month = +prompt("请输入有几个季度需要统计")
+let sum = []
+for (let i = 1; i <= month; i++) {
+  sum.push(+prompt(`请输入第${i}季度的收入`))
+}
+document.write(`<div class="box">`)
+for (let i = 0; i < sum.length; i++) {
+  document.write(
+    `<div style="height: ${sum[i]};">
+      <span>${sum[i]}</span>
+      <h4>${i + 1}</h4>
+    </div>`
+  )
+}
+document.write(`</div>`)
