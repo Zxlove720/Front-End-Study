92ccbeb8140b8f0604543ee81fa8b92b24113fb7
wzb <2482924813@qq.com>
2025-10-20 15:53:21 +0800

    JavaScript函数练习

--- js/js/JavaScript函数/01.函数练习.js
+++ js/js/JavaScript函数/01.函数练习.js
@@ -1,7 +1,34 @@
 // 返回两个数之和
-let a = +prompt("请输入第一个数")
-let b = +prompt("请输入第二个数")
-function sum(a = 0, b = 0) {
-  return a + b
+// let a = +prompt("请输入第一个数")
+// let b = +prompt("请输入第二个数")
+// function sum(a = 0, b = 0) {
+//   return a + b
+// }
+// document.writeln(`${a} + ${b} = ${sum(a, b)}`)
+
+
+// 求数组最值
+function max(array = []) {
+  let max = array[0]
+  for (let i = 0; i < array.length - 1; i++) {
+    max = max > array[i + 1] ? max : array[i + 1]
+  }
+  return max
 }
-document.writeln(`${a} + ${b} = ${sum(a, b)}`)
+
+function min(array = []) {
+  let min = array[0]
+  for (let i = 0; i < array.length - 1; i++) {
+    min = min < array[i + 1] ? min : array[i + 1]
+  }
+  return min
+}
+
+
+let n = +prompt("要输入几个数字？")
+let array = []
+for (let i = 0; i < n; i++) {
+  array.push(+prompt(`请输入第${i + 1}个数`))
+}
+console.log(max(array))
+console.log(min(array))
