e47733036efcb9b28ae28e6cc15b0963703ec138
wzb <2482924813@qq.com>
2025-10-23 23:31:53 +0800

    JavaScript内置对象

---
+++ js/js/04.JavaScript对象/04.内置对象.js
@@ -0,0 +1,20 @@
+console.log(Math.ceil(1.2)) // 输出2
+console.log(Math.ceil(-1.2)) // 输出-1
+// ceil n.天花板 这个方法是向上（X的正轴）取整
+
+console.log(Math.floor(1.9)) // 输出1
+console.log(Math.floor(-1.9)) // 输出-2
+// floor n.地板 这个方法是将数字向下（X的负轴方向）取整
+
+console.log(Math.round(1.4)) // 输出1
+console.log(Math.round(1.5)) // 输出2
+console.log(Math.round(-1.4)) // 输出-1
+console.log(Math.round(-1.51)) // 输出-2
+console.log(Math.round(-1.5)) // 输出-1
+
+let obj = {
+  username: "zhangsan",
+  password: "123456",
+  password: "654321"
+}
+console.log(obj.password) // 输出654321，后写的覆盖先写的
