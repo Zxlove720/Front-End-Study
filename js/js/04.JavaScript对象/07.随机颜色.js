8c633b9b7f86f8e2b3b6ee54c609ccc720d66bc4
wzb <2482924813@qq.com>
2025-10-27 17:08:15 +0800

    JavaScript随机颜色

---
+++ js/js/04.JavaScript对象/07.随机颜色.js
@@ -0,0 +1,18 @@
+function getRandomColor(flag) {
+  let prefix = "#"
+  let color = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+  if (flag) {
+    for (let i = 0; i < 6; i++) {
+      prefix = prefix + color[Math.floor(Math.random() * color.length)]
+    }
+    return prefix
+  } else {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+  }
+}
+let choice = [true, false]
+const div = document.querySelector("div")
+div.style.backgroundColor = getRandomColor(choice[Math.floor(Math.random() * choice.length)])
