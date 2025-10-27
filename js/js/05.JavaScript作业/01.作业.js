af37b78b37196c518d583b74785c7d17a72128c0
wzb <2482924813@qq.com>
2025-10-27 22:23:40 +0800

    JavaScript作业

---
+++ js/js/05.JavaScript作业/01.作业.js
@@ -0,0 +1,10 @@
+// const num = 10
+// console.log(num + 11) // 21
+// console.log(num + '11') // 1011 
+// console.log(num + +'11') // 21
+
+
+const num = 10
+console.log(typeof num + '11') // number11
+console.log(typeof (num + '11')) // string
+console.log(typeof (num + +'11')) // number
