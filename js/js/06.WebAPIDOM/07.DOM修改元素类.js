f5ca336c8e77cff32968d2b6091deb81fc929cde
wzb <2482924813@qq.com>
2025-11-12 00:17:19 +0800

    JavaScriptDOM修改元素类

--- js/js/06.WebAPIDOM/07.DOM修改元素类.js
+++ js/js/06.WebAPIDOM/07.DOM修改元素类.js
@@ -1,7 +1,7 @@
 // 假如元素需要修改的属性过多，在JavaScript通过对象属性进行修改就有些复杂，可以通过CSS写一个新类，然后通过
 // JavaScript修改元素的类即可
 const box = document.querySelector(`.box`)
-// 如果元素本来有class，此时就会覆盖掉原来的类
-box.className = `new`
-// 也可以保留多个类，此时就遵循CSS的覆盖规则
-box.className = `new box`
+// 获取到元素的classList，然后用add方法追加一个心类
+box.classList.add(`new`)
+box.classList.remove(`box`)
+box.classList.toggle(`new`)
