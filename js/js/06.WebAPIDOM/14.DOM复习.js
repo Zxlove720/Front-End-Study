acb490b9edada25f4a112f40c640cc4b2bbbe0be
wzb <2482924813@qq.com>
2025-11-13 23:49:11 +0800

    JavaScriptDOM复习

--- js/js/06.WebAPIDOM/14.DOM复习.js
+++ js/js/06.WebAPIDOM/14.DOM复习.js
@@ -1,5 +1,5 @@
 // 删除数组 splice(起始索引, 删除个数)
-let array = [1, 2, 3, 4, 5]
+// let array = [1, 2, 3, 4, 5]
 // console.log(array)
 // array.splice(2) // 从索引2开始全部删除
 // console.log(array)
@@ -19,3 +19,9 @@ let array = [1, 2, 3, 4, 5]
 // // 相当于body标签
 // console.log(document.body)
 
+
+// // innerText和innerHTML
+// const div = document.querySelector(`div`)
+// // div.innerText = `文本！`
+// div.innerText = `<h1>文本</h1>` // 不会解析文本中的标签
+// div.innerHTML = `<h1>文本</h1>` // 会解析文本中的标签，一般使用innerHTML即可
