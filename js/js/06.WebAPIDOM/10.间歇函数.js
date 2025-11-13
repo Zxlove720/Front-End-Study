a74a1726c5b942aa28b9e18f082c8c82a58541a5
wzb <2482924813@qq.com>
2025-11-13 17:41:44 +0800

    JavaScript间歇函数

---
+++ js/js/06.WebAPIDOM/10.间歇函数.js
@@ -0,0 +1,34 @@
+// 间歇函数
+// let i = 0
+// let n = setInterval(function () {
+//   console.log(i)
+//   i++
+//   if (i === 10) {
+//     clearInterval(n)
+//   }
+// }, 500)
+
+// let timer1 = setInterval(function () {
+//   console.log(`间歇函数1`)
+// }, 500)
+
+// let timer2 = setInterval(function () {
+//   console.log(`间歇函数2`)
+// }, 500)
+
+// console.log(timer1);
+// console.log(timer2);
+
+// function test() {
+//   console.log(`helloworld1`)
+// }
+
+// function test2() {
+//   console.log(`helloworld2`)
+// }
+
+// let time1 = setInterval(test(), 500)
+// let time2 = setInterval(test2, 1000)
+
+
+
