67b7fd7bb194d2aac3297cdaa97eb6f9c3c8811a
wzb <2482924813@qq.com>
2025-11-13 23:29:41 +0800

    JavaScriptDOM复习

---
+++ js/js/06.WebAPIDOM/14.DOM复习.js
@@ -0,0 +1,21 @@
+// 删除数组 splice(起始索引, 删除个数)
+let array = [1, 2, 3, 4, 5]
+// console.log(array)
+// array.splice(2) // 从索引2开始全部删除
+// console.log(array)
+
+// splice不但可以删除元素，也可以添加元素
+// splice(起始索引, 替换个数, 替换元素)
+// 但是不推荐这样做
+// array.splice(0, 0, "pink")
+// console.log(array[0])
+// console.log(array);
+
+// console.log(document);
+// console.log(typeof document);
+
+// // 相当于HTML标签
+// console.log(document.documentElement)
+// // 相当于body标签
+// console.log(document.body)
+
