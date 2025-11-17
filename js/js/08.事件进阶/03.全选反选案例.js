7e7abb628a0b2bcd3041f027d9f0241e1acb7785
wzb <2482924813@qq.com>
2025-11-17 23:09:00 +0800

    JavaScript全选反选案例

--- js/js/08.事件进阶/03.全选反选案例.js
+++ js/js/08.事件进阶/03.全选反选案例.js
@@ -5,3 +5,15 @@ checkAll.addEventListener(`click`, function () {
     checkBox[i].checked = checkAll.checked
   }
 })
+
+for (let i = 0; i < checkBox.length; i++) {
+  checkBox[i].addEventListener(`click`, function () {
+    let checkedCount = 0
+    for (let j = 0; j < checkBox.length; j++) {
+      if (checkBox[j].checked) {
+        checkedCount++
+      }
+    }
+    checkAll.checked = (checkBox.length === checkedCount)
+  })
+}
