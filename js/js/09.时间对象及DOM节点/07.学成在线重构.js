bb1fbf84621a80c9060701879052b26d8a38fd37
wzb <2482924813@qq.com>
2025-11-21 22:32:40 +0800

    JavaScript学成在线重构

---
+++ js/js/09.时间对象及DOM节点/07.学成在线重构.js
@@ -0,0 +1,66 @@
+// 1. 重构  
+let data = [
+  {
+    src: 'images/course01.png',
+    title: 'Think PHP 5.0 博客系统实战项目演练',
+    num: 1125
+  },
+  {
+    src: 'images/course02.png',
+    title: 'Android 网络动态图片加载实战',
+    num: 357
+  },
+  {
+    src: 'images/course03.png',
+    title: 'Angular2 大前端商城实战项目演练',
+    num: 22250
+  },
+  {
+    src: 'images/course04.png',
+    title: 'Android APP 实战项目演练',
+    num: 389
+  },
+  {
+    src: 'images/course05.png',
+    title: 'UGUI 源码深度分析案例',
+    num: 124
+  },
+  {
+    src: 'images/course06.png',
+    title: 'Kami2首页界面切换效果实战演练',
+    num: 432
+  },
+  {
+    src: 'images/course07.png',
+    title: 'UNITY 从入门到精通实战案例',
+    num: 888
+  },
+  {
+    src: 'images/course08.png',
+    title: 'Cocos 深度学习你不会错过的实战',
+    num: 590
+  },
+]
+
+// 获取父元素
+const box = document.querySelector(`.clearfix`)
+// 根据数据个数创建li
+for (let i = 0; i < data.length; i++) {
+  // 创建元素li
+  const li = document.createElement(`li`)
+  // li设置内容
+  li.innerHTML = `
+  <a href="#">
+    <img src=${data[i].src} alt="${data[i].title}">
+      <h4>
+        ${data[i].title}
+      </h4>
+      <div class="info">
+        <span>高级</span> • <span>${data[i].num}</span>人在学习
+      </div>
+  </a>
+  `
+
+  // 将li追加到父元素中
+  box.appendChild(li)
+}
