f6ba61bb1e35b535f86e051ebfa226b9aaf87af6
wzb <2482924813@qq.com>
2025-11-21 21:26:56 +0800

    JavaScriptDOM节点

--- js/js/09.时间对象及DOM节点/04.DOM节点.js
+++ js/js/09.时间对象及DOM节点/04.DOM节点.js
@@ -1,13 +1,24 @@
-// DOM树中每一个内容都称为一个DOM节点
-// 1.元素节点   各类标签    主要使用的也是元素节点，获得了元素其他都可以获得
-// 2.属性节点   标签的属性
-// 3.文本节点   标签的文本
+(function () {
+  // DOM树中每一个内容都称为一个DOM节点
+  // 1.元素节点   各类标签    主要使用的也是元素节点，获得了元素其他都可以获得
+  // 2.属性节点   标签的属性
+  // 3.文本节点   标签的文本
 
-// 查找节点（都是通过属性查找的）
-// 通过各个节点的关系查找节点
-const son1 = document.querySelector(`.son1`)
-// parentNode返回该节点的最近一级的父节点
-const father = son1.parentNode
-console.log(father)
+  // 查找节点（都是通过属性查找的）
+  // 通过各个节点的关系查找节点
+  const son1 = document.querySelector(`.son1`)
+  // parentNode返回该节点的最近一级的父节点
+  const father = son1.parentNode
+  console.log(father)
+})();
 
 
+(function () {
+  const father = document.querySelector(`.father`)
+  // 父节点获取所有子节点（包括属性和文本节点）
+  const children = father.childNodes
+  console.log(children);
+  // 父节点获取所有元素子节点，该方法使用较多
+  const childList = father.children
+  console.log(childList);
+})()
