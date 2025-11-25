d840c9411dcd3125a8dc479f89bf40ab909376a3
wzb <2482924813@qq.com>
2025-11-25 16:33:58 +0800

    JavaScript正则修饰符

--- js/js/11.正则表达式/04.正则修饰符.js
+++ js/js/11.正则表达式/04.正则修饰符.js
@@ -6,3 +6,10 @@ console.log(/JAVA/g.test(`javaJAVAjaVA`))
 // 字符串的match方法和正则表达式的exec方法类似
 // 修饰符g会进行全局匹配
 console.log(`javaJAVAjaVa`.match(/JAVA/ig))
+
+// 替换符合正则表达式的内容
+// replace()方法
+const regex = /前端/g
+const string = `前端开发是正确的，前端开发可以月薪过万`
+const newString = string.replace(regex, `JAVA`)
+console.log(newString);
