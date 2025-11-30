bc1b4b4dd85edf131a9fd4ad29226b2f74124456
wzb <2482924813@qq.com>
2025-11-30 10:58:23 +0800

    JavaScript综合案例首页

--- js/12.综合案例/js/index.js
+++ js/12.综合案例/js/index.js
@@ -3,22 +3,31 @@
 const login = document.querySelector(`.xtx_navs li`)
 const register = login.nextElementSibling
 render()
+
 // 渲染函数
 function render() {
   // 获取localStorage中的数据
   const username = localStorage.getItem(`username`)
   // localStorage中没有信息
   if (username) {
-
-    return
+    // localStorage中有信息，完成渲染
+    login.innerHTML = `<a>${username}</a>`
+    register.innerHTML = `<a>退出登录</a>`
+  } else {
+    login.innerHTML = `<a href="./login.html">请先登录</a>`
+    register.innerHTML = `<a href="./register.html">免费注册</a>`
   }
-  // localStorage中有信息，完成渲染
-  login.innerHTML = username
-  register.innerHTML = `退出登录`
 }
 
 // 退出登录，删除localStorage中的数据，并且再次渲染
-register.addEventListener(`click`, function () {
-  localStorage.removeItem(`username`)
-  render()
+register.addEventListener('click', function (e) {
+  const username = localStorage.getItem('username')
+  if (username) {
+    e.preventDefault()
+    localStorage.removeItem('username')
+
+    render()
+    location.reload()
+  }
+  // 未登录状态允许正常跳转
 })
