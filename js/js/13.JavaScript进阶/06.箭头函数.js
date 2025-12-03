cff34ef9cf5d5199692b9f9fcaa39c91f99cc9d5
wzb <2482924813@qq.com>
2025-12-03 23:40:09 +0800

    JavaScript箭头函数

--- js/js/13.JavaScript进阶/06.箭头函数.js
+++ js/js/13.JavaScript进阶/06.箭头函数.js
@@ -33,5 +33,49 @@
 // const fn = x => console.log(arguments) // 箭头函数中没有arguments，所以说报错arguments is not defined
 // fn()
 // 箭头函数只有剩余参数 使用剩余参数不能省略()
-const fn = (...x) => console.log(x)
-fn(1, 2, 3, 4, 5)
+// const fn = (...x) => console.log(x)
+// fn(1, 2, 3, 4, 5)
+
+
+// 箭头函数没有this
+// 箭头函数不会创建自己的this，只会从自己的作用域链的上一层沿用this，直到window
+// console.log(this) // 函数调用者是window，所以说这里的this指向window
+
+// const simple = function () {
+//   console.log(this)
+// }
+// simple() // 函数调用者是window，所以说这里的this指向window
+
+// document.documentElement.addEventListener(`click`, function () {
+//   // this指向HTML元素
+//   console.log(this)
+// })
+
+// document.documentElement.addEventListener(`click`, () => {
+//   // 因为箭头函数没有this，它会从自己的作用域链的上一层沿用this，所以说this指向window
+//   console.log(this)
+// })
+
+// const user = {
+//   username: `jack`,
+//   walk: () => console.log(this)
+// }
+// // this指向window
+// 因为walk是箭头函数，没有自己的this，所以说会从自己作用域链上一层沿用this，对象没有this，所以说会从window中沿用this，指向window
+// user.walk()
+
+
+const user = {
+  username: `jack`,
+  sleep: function () {
+    console.log(this)
+    const fn = () => console.log(this)
+    fn()
+  }
+}
+// 普通函数作为对象方法被调用时，this指向调用它的对象
+// sleep是普通函数，所以说this指向调用它的对象
+user.sleep()
+
+// 因为箭头函数没有this，所以说即使DOM事件是匿名函数，但是因为DOM事件回调函数有时需要this，所以说不建议使用箭头函数作为DOM事件回调函数
+
